3d78335e36c81d4292712ddd61550529138d8a49
lugope <lucasgpereira.10@gmail.com>
2018-06-22 11:28:45 -0300

    Setting up unit tests.

---
+++ diskspace/tests/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class DiskSpaceTest(unittest.TestCase):
+	"""docstring for DiskSpaceTest"""
+	def test_test(self):
+		value = 1;
+		self.assertEqual(1, value)
+
+if __name__ == "__main__":
+	unittest.main()
